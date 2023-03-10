fb3b4c554e2dff096ef55a0576210421f40d3f05
sinchang <sinchangwen@gmail.com>
2023-03-11 00:19:45 +0800

    basic button

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -1,14 +1,20 @@
-const path = require('path')
+/** @type { import('@storybook/react-vite').StorybookConfig } */
 module.exports = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    '@storybook/addon-docs'
   ],
   core: { builder: '@storybook/builder-vite', disableTelemetry: true },
   docs: {
-    autodocs: 'tag',
+    autodocs: true, // see below for alternatives
+    defaultName: 'Docs', // set to change the name of generated docs entries
   },
-  framework: '@storybook/react-vite'
+  framework: '@storybook/react-vite',
+  typescript: {
+    // also valid 'react-docgen-typescript' | false
+    reactDocgen: 'react-docgen',
+  },
 }
