7eab1914ccc022265071c3eef68309294b82d492
sinchang <sinchangwen@gmail.com>
2023-03-23 14:20:37 +0800

    feat: support leftIcon and rightIcon prop on the Button component

--- packages/ui/src/button/Button.tsx
+++ packages/ui/src/button/Button.tsx
@@ -1,11 +1,14 @@
 import styles from './button.module.scss'
 import classNames from 'classnames'
+import { ReactNode } from 'react'
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: 'primary' | 'outline' | 'ghost' | 'link'
   size?: 'large' | 'medium' | 'small'
   danger?: boolean
+  leftIcon?: ReactNode
+  rightIcon?: ReactNode
 }
 
 export const Button = ({
@@ -14,6 +17,8 @@ export const Button = ({
   children,
   className,
   danger = false,
+  leftIcon,
+  rightIcon,
   ...props
 }: ButtonProps) => {
   const btnClass = classNames(className, {
@@ -24,7 +29,9 @@ export const Button = ({
 
   return (
     <button className={btnClass} {...props}>
+      {leftIcon ? <span className={styles.leftIcon}>{leftIcon}</span> : null}
       {children}
+      {rightIcon ? <span className={styles.rightIcon}>{rightIcon}</span> : null}
     </button>
   )
 }
